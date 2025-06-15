77dbc6741eb9968423b8dd5847dc65eb08b60678
Sukesh Bhunia <126257085+ContactSukesh@users.noreply.github.com>
2025-06-15 14:57:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def send_message():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app,allow_unsafe_werkzeug=True, host='0.0.0.0', port=port)
